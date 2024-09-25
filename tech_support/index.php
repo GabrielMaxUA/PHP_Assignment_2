eaade7797a92b0c8ef7400f9f4e44a63dc8af951
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-09-25 19:52:06 -0400

    add new css code

--- tech_support/index.php
+++ tech_support/index.php
@@ -1,38 +1,33 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="main.css">
-</head>
-<body>
 <?php include 'view/header.php'; ?>
 <main>
     <nav>
-        
+    <div class="admin">
     <h2>Administrators</h2>
     <ul>
-        <li><a href="product_manager">Manage Products</a></li>
+        <li><a href="under_construction.php">Manage Products</a></li>
         <li><a href="under_construction.php">Manage Technicians</a></li>
         <li><a href="under_construction.php">Manage Customers</a></li>
         <li><a href="under_construction.php">Create Incident</a></li>
         <li><a href="under_construction.php">Assign Incident</a></li>
         <li><a href="under_construction.php">Display Incidents</a></li>
     </ul>
-
+    </div>
+   
+    <div class="tech">
     <h2>Technicians</h2>    
     <ul>
         <li><a href="under_construction.php">Update Incident</a></li>
     </ul>
-
-    <h2>Customers</h2>
+    </div>
+    
+<div class="customer">
+<h2>Customers</h2>
     <ul>
         <li><a href="under_construction.php">Register Product</a></li>
     </ul>
+</div>
+   
     
     </nav>
-</section>
+</main>
 <?php include 'view/footer.php'; ?>
-</body>
-</html>
