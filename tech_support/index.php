4eeefa2d51ebf1216a4e5add4d8c042449e374f0
Max Gabriel <Max.Gabriel@triosstudent.com>
2024-09-26 23:22:48 -0400

    added the product registration

--- tech_support/index.php
+++ tech_support/index.php
@@ -7,7 +7,7 @@
         <li><a href="product_manager/index.php">Manage Products</a></li>
         <li><a href="technician_manager/index.php">Manage Technicians</a></li>
         <li><a href="customer_manager/index.php">Manage Customers</a></li>
-        <li><a href="under_construction.php">Create Incident</a></li>
+        <li><a href="incident/customer_search.php">Create Incident</a></li>
         <li><a href="under_construction.php">Assign Incident</a></li>
         <li><a href="under_construction.php">Display Incidents</a></li>
     </ul>
@@ -23,7 +23,7 @@
     <div class="customer">
     <h2>Customers</h2>
         <ul>
-            <li><a href="under_construction.php">Register Product</a></li>
+            <li><a href="product_registration/login_page.php">Register Product</a></li>
         </ul>
     </div>
    
